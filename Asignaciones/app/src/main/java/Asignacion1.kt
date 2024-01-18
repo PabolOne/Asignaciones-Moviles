efab7e450a62d651ca2c157d7426468379710902
PabolOne <luis.ayon233145@potros.itson.edu.mx>
2024-01-18 12:35:20 -0700

    Asignacion1

---
+++ Asignaciones/app/src/main/java/Asignacion1.kt
@@ -0,0 +1,29 @@
+class Asignacion1 {
+    fun main(args: Array<String>) {
+        println(verificaCadena("nombre")) // true
+        println(verificaCadena("_nombre")) // true
+        println(verificaCadena("_12")) // true
+        println(verificaCadena("")) // false
+        println(verificaCadena("012")) // false
+        println(verificaCadena("no$")) // false
+    }
+
+    fun verificaCadena(cadena: String): Boolean {
+        if (cadena.isEmpty()) {
+            return false
+        }
+
+        val primerCaracter = cadena[0]
+        if (!(primerCaracter.isLetter() || primerCaracter == '_')) {
+            return false
+        }
+
+        for (caracter in cadena) {
+            if (!(caracter.isLetterOrDigit() || caracter == '_')) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
